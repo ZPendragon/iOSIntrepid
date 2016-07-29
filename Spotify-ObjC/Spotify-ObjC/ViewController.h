2cbd31d58e61c3874366c2e97ad6d57b1ef3c823
Kevin Zeckser <kevinzeckser@Kevins-MacBook-Pro-2.local>
2016-07-29 10:40:51 -0400

    Initial project commit.

---
+++ Spotify-ObjC/Spotify-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify-ObjC
+//
+//  Created by Kevin Zeckser on 7/29/16.
+//  Copyright © 2016 Kevin Zeckser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Spotify-ObjC/Spotify-ObjC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Spotify-ObjC
+//
+//  Created by Kevin Zeckser on 7/29/16.
+//  Copyright © 2016 Kevin Zeckser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
